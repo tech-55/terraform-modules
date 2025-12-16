c9b5c38f5343c9d04489762d4ef3fbad60004944
Ivan Meireles <ivanmds2@hotmail.com>
2025-12-16 11:24:51 -0300

    adjusts

--- eks-service-account/variables.tf
+++ eks-service-account/variables.tf
@@ -2,14 +2,13 @@ variable "aws_region" {
     description = "The AWS region to deploy resources in"
     type        = string
 }
-variable "aws_account" {}
 
 variable "namespace" {
     description = "The Kubernetes namespace where the service account is located"
     type        = string
 }
 
-variable "app_name" {
+variable "service_account_name" {
     description = "The name of the Kubernetes service account"
     type        = string
 }
