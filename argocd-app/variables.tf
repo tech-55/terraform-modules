7e69e2706a2a4c92d99a91b13cb7a7e4705a19ff
Ivan Meireles <ivanmds2@hotmail.com>
2025-12-18 14:50:18 -0300

    adjusts

--- argocd-app/variables.tf
+++ argocd-app/variables.tf
@@ -11,7 +11,7 @@ variable "github_repo_url" {}
 
 variable "argocd_sources" {
   type = object({
-    helmTargetRevision = optional(string, "0.1.21")
+    helmTargetRevision = optional(string, "0.1.22")
     helmValues     = string
     branch         = string
   })
