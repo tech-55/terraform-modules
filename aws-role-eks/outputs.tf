4652b744bc3bd00d7f4a623e14a53802ad87b57a
Ivan Meireles <ivanmds2@hotmail.com>
2026-01-06 16:31:31 -0300

    adjusts

--- aws-role-eks/outputs.tf
+++ aws-role-eks/outputs.tf
@@ -1,6 +1,11 @@
 output "role_arn" {
   value = aws_iam_role.service_account_role.arn
 }
+
+output "role_name" {
+  value = aws_iam_role.service_account_role.name
+}
+
 output "policy_arn" {
     value = aws_iam_policy.service_account_policy.arn
 }
