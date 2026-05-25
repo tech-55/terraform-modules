331050918253cd3cd6f8ab2a02614a80364ea542
Ivan Meireles <ivanmds2@hotmail.com>
2026-05-25 11:00:51 -0300

    adjusts

--- eks-service-account/outputs.tf
+++ eks-service-account/outputs.tf
@@ -1,6 +1,11 @@
 output "role_arn" {
   value = module.create_aws_role_eks.role_arn
 }
+
 output "policy_arn" {
     value = module.create_aws_role_eks.policy_arn
+}
+
+output "service_account_name" {
+    value = module.create_aws_role_eks.service_account_name
 }
